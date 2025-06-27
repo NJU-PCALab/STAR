629c7ab556b4233f813deee492ea22c9eb7d3a22
CSRuiXie <165138837+CSRuiXie@users.noreply.github.com>
2025-06-27 18:28:55 +0800

    Update train_sr.sh

--- video_super_resolution/scripts/train_sr.sh
+++ video_super_resolution/scripts/train_sr.sh
@@ -2,13 +2,13 @@
 
 torchrun \
   --nnodes=1 \
-  --nproc_per_node=1 \
+  --nproc_per_node=8 \
   ./video_super_resolution/scripts/train_sr.py \
-  --pretrained_model_path '/mnt/bn/videodataset-uswest/VSR/pretrained_models/venhancer/venhancer_paper.pt' \
+  --pretrained_model_path '' \
   --train_batch_size 1 \
   --max_train_steps 15000 \
   --checkpointing_steps 500 \
   --learning_rate 5e-5 \
-  --train_data_dir '/mnt/bn/videodataset-uswest/VSR/dataset/SRTraining/training_group_0' \
+  --train_data_dir '' \
   --num_frames 32 \
-  --output_dir '/mnt/bn/videodataset-uswest/VSR/exp/venhancer/debug'
+  --output_dir ''
